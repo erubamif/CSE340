46a9c0c3c1f44a3b2f5cf9788aca6962e301ffe0
ferudenis <ferudenis@gmail.com>
2026-09-10 16:47:43 +0100

    categories page added

--- server.js
+++ server.js
@@ -48,3 +48,9 @@ app.get('/projects', async (req, res) => {
     const title = 'Service Projects';
     res.render('projects', { title });
 });
+
+
+app.get('/categories', async (req, res) => {
+    const title = 'Service Project Categories';
+    res.render('categories', { title });
+});
