e3a6f13786abda86443a41345a6c4d2b34cac3c3
ferudenis <ferudenis@gmail.com>
2026-09-11 22:37:10 +0100

    add

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 import { testConnection } from './src/models/db.js';
 import { getAllOrganizations } from './src/models/organizations.js';
+import { getAllProjects } from './src/models/projects.js';
 
 const NODE_ENV = 'production';
 const PORT = 3000;
@@ -55,8 +56,11 @@ app.get('/organizations', async (req, res) => {
 });
 
 app.get('/projects', async (req, res) => {
+    const projects = await getAllProjects();
+    console.log(projects);
+
     const title = 'Service Projects';
-    res.render('projects', { title });
+    res.render('projects', { title, projects });
 });
 
 
--- src/views/setup.sql
+++ src/views/setup.sql
@@ -5,3 +5,12 @@ CREATE TABLE organization (
     contact_email VARCHAR(255) NOT NULL,
     logo_filename VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE project (
+    project_id SERIAL PRIMARY KEY,
+    organization_id INTEGER NOT NULL REFERENCES organization(organization_id),
+    title VARCHAR(150) NOT NULL,
+    description TEXT NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    project_date DATE NOT NULL
+);
