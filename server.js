ee6259fe218c3aec37fdde432d4b574a0f3d60f1
ferudenis <ferudenis@gmail.com>
2026-09-16 08:49:49 +0100

    Fix NODE_ENV middleware

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import path from 'path';
 import { testConnection } from './src/models/db.js';
 import router from './src/routes.js';
 
+
 const NODE_ENV = 'production';
 const PORT = 3000;
 
@@ -45,6 +46,12 @@ app.use((req, res, next) => {
         console.log(`${req.method} ${req.url}`);
     }
     next(); // Pass control to the next middleware or route
+});
+
+// Middleware to make NODE_ENV available to all templates
+app.use((req, res, next) => {
+    res.locals.NODE_ENV = NODE_ENV;
+    next();
 });
 
 // Use the imported router to handle routes
