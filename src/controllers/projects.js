db3fc5ec980f67949bfd27e634382c88ccae691e
ferudenis <ferudenis@gmail.com>
2026-09-16 09:07:41 +0100

    add

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -11,3 +11,26 @@ const showProjectsPage = async (req, res) => {
 
 // Export any controller functions
 export { showProjectsPage };
+
+const getProjectsByOrganizationId = async (organizationId) => {
+      const query = `
+        SELECT
+          project_id,
+          organization_id,
+          title,
+          description,
+          location,
+          date
+        FROM project
+        WHERE organization_id = $1
+        ORDER BY date;
+      `;
+      
+      const queryParams = [organizationId];
+      const result = await db.query(query, queryParams);
+
+      return result.rows;
+};
+
+// Export the model functions
+export { getAllProjects, getProjectsByOrganizationId };
